825526a48929c0b7a6065a0a5016e5e0e07aacbd
Enric Cervera <ecervera@uji.es>
2019-11-26 10:01:43 +0100

    stereo_bm not working

--- src/stereo_bm/cpu_processing.cpp
+++ src/stereo_bm/cpu_processing.cpp
@@ -1,17 +1,34 @@
 #include <opencv2/opencv.hpp>
+#include <opencv2/ximgproc.hpp>
 
 void cpu_processing(cv::Mat& src1, cv::Mat& src2, cv::Mat& dst)
 {
-  const int lowThreshold = 20;
-  const int ratio = 3;
-  const int kernel_size = 3;
-  cv::Mat src_gray, detected_edges;
+  int max_disp = 64;
+  int wsize = 15;
+  double vis_mult = 1.0;
+  double lambda = 8000.0;
+  double sigma = 1.5;
 
-  cv::cvtColor( src1, src_gray, cv::COLOR_BGR2GRAY );
-  cv::blur( src_gray, detected_edges, cv::Size(3,3) );
-  cv::Canny( detected_edges, detected_edges, lowThreshold, 
-             lowThreshold*ratio, kernel_size );
-  dst = cv::Scalar::all(0);
-  src1.copyTo( dst, detected_edges );
+  cv::Mat left_disp, right_disp, filtered_disp, filtered_disp_vis;
+
+  cv::Ptr<cv::StereoBM> left_matcher = cv::StereoBM::create(max_disp,wsize);
+  cv::Ptr<cv::ximgproc::DisparityWLSFilter> wls_filter = cv::ximgproc::createDisparityWLSFilter(left_matcher);
+  cv::Ptr<cv::StereoMatcher> right_matcher = cv::ximgproc::createRightMatcher(left_matcher);
+
+  src1.convertTo(src1, CV_8U);
+  cv::cvtColor(src1, src1,  cv::COLOR_BGR2GRAY);
+  src2.convertTo(src2, CV_8U);
+  cv::cvtColor(src2, src2,  cv::COLOR_BGR2GRAY);
+  
+  left_matcher->compute(src1, src2, left_disp);
+  right_matcher->compute(src2, src1, right_disp);
+
+  wls_filter->setLambda(lambda);
+  wls_filter->setSigmaColor(sigma);
+  wls_filter->filter(left_disp, src1, filtered_disp, right_disp);
+
+  cv::ximgproc::getDisparityVis(left_disp, filtered_disp_vis, vis_mult);
+  //cv::ximgproc::getDisparityVis(filtered_disp, filtered_disp_vis, vis_mult);
+  cv::cvtColor(filtered_disp_vis, dst, cv::COLOR_GRAY2BGR);
 }
 
